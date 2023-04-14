060ccf7dcfa243fe3383146ba274c3c63b7ce496
Aldiansyah <aldiansyah.ramadlan@asyx.com>
2023-04-14 12:49:18 +0700

    generate number

--- src/main/java/asyx/sharing/simplebackend/service/BankService.java
+++ src/main/java/asyx/sharing/simplebackend/service/BankService.java
@@ -30,64 +30,6 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class BankService {
-
-	public static ObjectWriter objectWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();
-
-	public ResponseEntity<Object> bankAdd(RequestBankAdd request) throws IOException, InterruptedException {
-//		String writeValueAsString = objectWriter.writeValueAsString(request);
-//		log.info("data : " + writeValueAsString);
-//		log.info("bankCode, " + request.getBankCode());
-
-		if (request.getBankCode().equals("000"))
-			throw new CustomGenericException("cannot use bank code 000");
-
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
-
-	public ResponseEntity<Object> getDataExternal() throws InterruptedException, IOException {
-		HttpRequest request = 
-				HttpRequest.newBuilder()
-				.uri(URI.create("https://api.rajaongkir.com/starter/province"))
-				.headers("key", "766038aafa282632fb8bd394dd49102b").GET().build();
-
-
-		HttpResponse<String> responseHttp = 
-				HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build()
-				.send(request, BodyHandlers.ofString());
-
-		return new ResponseEntity<>(responseHttp.body(), HttpStatus.OK);
-	}
-
-	@Cacheable("bank")
-	public ResponseEntity<Object> getBankInfo(String bankCode) throws InterruptedException, IOException {
-		log.info("----Sleep for 4 Secs.. backend call.-----");
-		Thread.sleep(1000 * 4);
-		return new ResponseEntity<>(bankCode, HttpStatus.OK);
-	}
-
-	@CacheEvict(value = "bank", allEntries = true)
-	public ResponseEntity<Object> updateBankInfo(String bankCode) throws InterruptedException, IOException {
-		return new ResponseEntity<>(bankCode, HttpStatus.OK);
-	}
-	
-	
-	public static void fakestoreapi() throws IOException, InterruptedException {
-		HttpRequest request = 
-				HttpRequest.newBuilder()
-				.uri(URI.create("https://fakestoreapi.com/products")).GET().build();
-
-		HttpResponse<String> responseHttp = 
-				HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build()
-				.send(request, BodyHandlers.ofString());
-		log.info("product : {} ", responseHttp.body());
-		ObjectMapper objectMapper = new ObjectMapper();
-		FakestoreapiProduct nemo = new FakestoreapiProduct();
-		JsonFactory jfactory = new JsonFactory();
-//		objectMapper.readValue(new File("c:\\user.json"), Rating.class);
-
-		
-	}
-	
 	
 	public ResponseEntity<Object> numberGenerate(int numb) {
 		String numbTemp = String.valueOf(numb);
